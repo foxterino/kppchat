a71b4077359c4ddfc94d053ad81eca31f200cc98
Foxterino <foxterinogg@gmail.com>
2020-04-11 02:31:25 +0300

    Add login logic

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { authSelectors, login } from '../../state/ducks/Auth';
 import { Routes } from '../Routes';
+import { Field } from '../../commons/components/Field';
 
 export const Login: React.FC = () => {
   const isAuthorized = useSelector(authSelectors.isAuthorized);
+  const [username, setUsername] = useState('');
+
   const dispatch = useDispatch();
 
-  const handleLogin = () => {
-    dispatch(login('my username'));
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handleLogin = (event: React.FormEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    dispatch(login(username));
   };
 
   return isAuthorized ? (
     <Redirect to={Routes.Dashboard} />
   ) : (
     <div>
-      login <button onClick={handleLogin}>login and go to dashboard</button>
+      <form>
+        <Field
+          value={username}
+          onChange={handleChange}
+          label="Username field"
+          id="username-field"
+          type="text"
+        />
+        <input value="Login" onClick={handleLogin} type="submit" />
+      </form>
     </div>
   );
 };
