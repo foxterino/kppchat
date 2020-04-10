ce519dd97c158d4b4e2976d177d57261ec28de14
Foxterino <foxterinogg@gmail.com>
2020-04-11 02:25:39 +0300

    Remove Loader

--- src/commons/components/Loader/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface LoaderProps {
-  isLoading?: boolean;
-}
-
-export const Loader: React.FC<LoaderProps> = ({ isLoading = true }) => {
-  return isLoading ? <div>Loading...</div> : null;
-};
